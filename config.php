6df0c122f99a39b44c94bdc70f9f0e5e22606bae
gz3ln3n3 <gz3ln3n3@gmail.com>
2015-06-17 23:21:38 -0430

    Server
    server

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php 
+/** 
+* Database config variables 
+*/ 
+define("DB_HOST", "mysql3.000webhost.com"); 
+define("DB_USER", "a4668813_admin"); 
+define("DB_PASSWORD", "admin123"); 
+define("DB_DATABASE", "a4668813_rye"); 
+/* 
+* Google API Key 
+*/ 
+define("GOOGLE_API_KEY", "AIzaSyDdFOCwwGdEdNN_v6niv0v6Ts3nF6pZn8U"); // Coloca tu Api key aquí 
+?>
